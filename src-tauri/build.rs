fa29fb9e02de72338a134dd787c6e216a78fe13b
David <davidstulemeijer@gmail.com>
2024-08-19 18:05:21 +0200

    Replaced copyThings.bat with OS independent alternative

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -24,5 +24,20 @@ fn main() {
         println!("cargo:include={}", resourcelib_dir.join("include").display());
     }
 
+    copy_static_assets();
     tauri_build::build();
+}
+
+fn copy_static_assets(){
+    let out_path = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("../build/_app/immutable/assets");
+    let static_folder = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("../static");
+
+    let files = vec![
+        "32px.png",
+        "throbber.gif",
+    ];
+
+    for file in files{
+        std::fs::copy(&static_folder.join(file), &out_path.join(file)).expect("Failed to copy dll to output directory: {}");
+    }
 }
